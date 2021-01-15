ab2126821c5419ba3ba13ee3117cd178ce2164b3
Dhruv Bhavsar <dhrv.bhvsr.557@gmail.com>
2021-01-15 21:34:59 +0530

    password bullet change simple case

--- Practice/2021-01-15/js/pass_bullets_2.js
+++ Practice/2021-01-15/js/pass_bullets_2.js
@@ -1,12 +1,22 @@
 // There are many cases to handle here so experimenting right now
 // will be done by tommorow
 
-document.getElementById('pass-placeholder').addEventListener('input', e => {
-    console.log(e);
-});
-
 // on submit shows the entered password and cancels submission
 document.getElementById('frm').addEventListener('submit', e => {
     alert(document.getElementById('password').value);
     return false;
 })
+
+// for the simple case when user only types password one way
+// and never presses backspace or delete or cut or paste or select text etc..
+// just straight forward type and submit without any mistakes....
+document.getElementById('pass-placeholder').addEventListener('input', e => {
+    console.log(e);
+    var ip = document.getElementById('pass-placeholder');
+    var pass = document.getElementById('password');
+
+    pass.value += e.data;
+    ip.value = '%'.repeat(ip.value.length);
+});
+
+// for more real-world like case
